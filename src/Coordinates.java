c4fa071afe4487adeb6a5810d258f54c1fb3976a
giannigrasso <gianni.grasso03@gmail.com>
2025-01-03 13:39:12 +0100

    add: coordinates class

--- src/Coordinates.java
+++ src/Coordinates.java
@@ -2,6 +2,26 @@
  * Class representing the position of a cell in the grid.
  */
 class Coordinates {
-    // ...
+    /**
+     * The coordinate row.
+     */
+    int row;
+
+    /**
+     * The coordinate column.
+     */
+    int col;
     
+    /**
+     * Coordinates constructor.
+     * 
+     * @param row the coordinate row
+     * @param col the coordinate column
+     */
+    Coordinates(int row, int col) {
+        if (row >= 0 && col >= 0) {
+            this.row = row;
+            this.col = col;
+        }
+    }
 }
